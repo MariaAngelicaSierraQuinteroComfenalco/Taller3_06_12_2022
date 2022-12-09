3d2db23076580da70dc5471ad01e8eab80d45f64
MariaAngelicaSierraQuinteroComfenalco <angelicasierr904@gmail.com>
2022-12-09 15:14:30 -0500

    solucion 2

--- main.js
+++ main.js
@@ -1,14 +1,17 @@
 addEventListener(`DOMContentLoaded`, (e)=>{
     
-    //Realizar un programa que permita leer 2 números diferentes y nos diga cual es el mayor de los 2 números.
-    let num1 = Number(prompt("Dame un numero",0));
-    let num2 = Number(prompt("Dame un numero diferente",0));
+    //Realice un programa que calcule el máximo de N números leídos desde teclado.
+    let c_numeros = Number(prompt("Cantidad de numeros a leer",0));
+    let numero_mayor=0
     
-         if (num1 > num2) { 
-            document.querySelector("body").innerHTML = "El numero mayor es "+num1
-         } else { 
-            document.querySelector("body").innerHTML = "El numero mayor es "+num2
-         }
-    
-    
+    for (let i = 0; i < c_numeros; i++) {
+        let num = Number(prompt("Dame un numero",0)); 
+        if(i==1){
+            numero_mayor=num
+        } else if(num > numero_mayor){
+            numero_mayor=num
+        
+        }
+    }
+    document.querySelector("body").innerHTML = "el numero mayor es: "+numero_mayor
     })

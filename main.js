e597900b76df176f7efedddcb19f088d48d7fb0f
MariaAngelicaSierraQuinteroComfenalco <angelicasierr904@gmail.com>
2022-12-09 15:17:28 -0500

    solucion 3

--- main.js
+++ main.js
@@ -1,17 +1,10 @@
 addEventListener(`DOMContentLoaded`, (e)=>{
     
-    //Realice un programa que calcule el máximo de N números leídos desde teclado.
-    let c_numeros = Number(prompt("Cantidad de numeros a leer",0));
-    let numero_mayor=0
+    //Realizar un programa que pueda sumar dos números leídos por teclado y escribir el resultado
+    let num1 = Number(prompt("dame un numero",0));
+    let num2 = Number(prompt("Dame un numero",0)); 
     
-    for (let i = 0; i < c_numeros; i++) {
-        let num = Number(prompt("Dame un numero",0)); 
-        if(i==1){
-            numero_mayor=num
-        } else if(num > numero_mayor){
-            numero_mayor=num
-        
-        }
-    }
-    document.querySelector("body").innerHTML = "el numero mayor es: "+numero_mayor
+    let suma=num1+num2
+    
+    document.querySelector("body").innerHTML = "la suma de  los dos numeros es :  "+suma
     })
